076763a1e3ba714d73ed2ca38cda461920fb873a
jrubacha <53633222+jrubacha@users.noreply.github.com>
2024-04-04 20:57:21 -0400

    candle

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -10,8 +10,11 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.motorcontrol.TalonFx;
 import frc.robot.motorcontrol.configurations.TalonFxConfiguration;
+import frc.robot.subsystems.CANdle.CANdleSubsystem;
+import frc.robot.subsystems.CANdle.Colors;
 
 public class IntakeSubsystem  extends SubsystemBase{
+    private CANdleSubsystem candle = new CANdleSubsystem();
 
     private final TalonFx m_rollerMotor = new TalonFx(
       Constants.Intake.Roller.rollerMotorID, 
@@ -74,9 +77,22 @@ public class IntakeSubsystem  extends SubsystemBase{
 
     }
 
+    public void runIntakeLEDs(){
+      if(isBeamBreakTriggered()) {
+        candle.setColor(Colors.GREEN);
+      } else {
+        candle.setCANdleToAllianceColor();
+      }
+    }
+
+    public void disableLEDs(){
+      candle.setColor(Colors.WHITE);
+    }
+    
     @Override
     public void periodic() {
         SmartDashboard.putNumber("Intake Roller: Current", m_rollerMotor.getStatorCurrent());
+        runIntakeLEDs();
       // This method will be called once per scheduler run
     }
   
